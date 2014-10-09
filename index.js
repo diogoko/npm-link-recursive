4203303d10499fadf75e6ce9848fa9ee617394f9
Diogo Kollross <diogoko@gmail.com>
2014-10-08 22:24:09 -0300

    Print success and error messages

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ var path = require('path');
 var walk = require('walkdir');
 
 function getRoot() {
-  return new Promise(function(resolve) {
-    exec('npm root -g'), function(error, stdout, stderr) {
+  return new Promise(function(resolve, reject) {
+    exec('npm root -g', function(error, stdout, stderr) {
+      if (error) {
+        reject(error);
+        return;
+      }
       resolve(stdout.trim());
     });
   });
@@ -23,19 +27,27 @@ function getLinkedPackages(prefix) {
 }
 
 function linkAll(linkedPackages) {
-  var walker = walk('.').on('directory', function(curPath, stat) {
-    var leaf = path.basename(curPath);
-    var leafParent = path.basename(path.dirname(curPath));
-    if (leafParent === 'node_modules' && linkedPackages.indexOf(leaf) !== -1) {
-      walker.pause();
-      linkPath(path.dirname(path.dirname(curPath)), leaf).then(function() {
-        walker.resume();
-      });
-    }
+  return new Promise(function(resolve, reject) {
+    var walker = walk('.').on('directory', function(curPath, stat) {
+      var leaf = path.basename(curPath);
+      var leafParent = path.basename(path.dirname(curPath));
+      if (leafParent === 'node_modules' && linkedPackages.indexOf(leaf) !== -1) {
+        walker.pause();
+        linkPath(path.dirname(path.dirname(curPath)), leaf).then(function() {
+          walker.resume();
+        }).catch(function(error) {
+          walker.end();
+          reject(error);
+        });
+      }
+    }).on('end', function() {
+      resolve();
+    });
   });
 }
 
 function linkPath(projectPath, packageName) {
+  console.log('linking ' + projectPath + '/node_modules/' + packageName);
   return npmUninstall(projectPath, packageName).
     then(function() {
       return npmLink(projectPath, packageName);
@@ -43,16 +55,24 @@ function linkPath(projectPath, packageName) {
 }
 
 function npmUninstall(cwd, packageName) {
-  return new Promise(function(resolve) {
+  return new Promise(function(resolve, reject) {
     exec('npm uninstall ' + packageName, { cwd: cwd }, function(error, stdout, stderr) {
+      if (error) {
+        reject(error);
+        return;
+      }
       resolve();
     });
   });
 }
 
 function npmLink(cwd, packageName) {
-  return new Promise(function(resolve) {
+  return new Promise(function(resolve, reject) {
     exec('npm link ' + packageName, { cwd: cwd }, function(error, stdout, stderr) {
+      if (error) {
+        reject(error);
+        return;
+      }
       resolve();
     });
   });
@@ -60,5 +80,7 @@ function npmLink(cwd, packageName) {
 
 getRoot().then(function(prefix) {
   var linkedPackages = getLinkedPackages(prefix);
-  linkAll(linkedPackages);
+  return linkAll(linkedPackages);
+}).catch(function(error) {
+  console.error(error.message);
 });
